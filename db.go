d6e1b4c290361db121193ed9c3778356aa6f73f5
Jairo <jairocaro@msn.com>
2021-03-04 22:39:26 +0100

    no uppercase on domains

--- db.go
+++ db.go
@@ -79,7 +79,7 @@ func (l *tLink) save() {
 }
 
 func randomString(n int) string {
-	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	var letters = []rune("abcdefghijklmnopqrstuvwxyz0123456789")
 
 	s := make([]rune, n)
 	for i := range s {
